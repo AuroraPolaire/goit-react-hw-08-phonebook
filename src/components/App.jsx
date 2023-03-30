d678b63c0b542640db79bc7e862d9c657c1efe95
AuroraPolaire <sea.forest.mountains@gmail.com>
2023-03-30 10:06:11 +0300

    fix: double userfetch removed - added checking in app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchCurrentUser } from 'redux/auth/operations';
 import Contacts from '../pages/Contacts';
 import { Route, Routes } from 'react-router-dom';
@@ -10,9 +10,11 @@ import ModalRegister from './ModalRegister/ModalRegister';
 import ModalLogin from './ModalLogin/ModalLogin';
 import Layout from './Layout';
 import { Toaster } from 'react-hot-toast';
+import { selectIsRefreshing } from 'redux/auth/selectors';
 
 export const App = () => {
   const dispatch = useDispatch();
+  const isRefreshing = useSelector(selectIsRefreshing);
 
   useEffect(() => {
     dispatch(fetchCurrentUser());
@@ -25,41 +27,43 @@ export const App = () => {
           top: 10,
         }}
       />
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route
-            path="/contacts"
-            element={
-              <PrivateRoute redirectTo="/login" component={<Contacts />} />
-            }
-          ></Route>
-          <Route
-            path="/register"
-            element={
-              <RestrictedRoute
-                redirectTo="/contacts"
-                component={<ModalRegister />}
-              />
-            }
-          ></Route>
-          <Route
-            path="/login"
-            element={
-              <RestrictedRoute
-                redirectTo="/contacts"
-                component={<ModalLogin />}
-              />
-            }
-          ></Route>
-          <Route
-            path="/logout"
-            element={<PrivateRoute redirectTo="/" component={<Home />} />}
-          ></Route>
-        </Route>
+      {isRefreshing ? null : (
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route
+              path="/contacts"
+              element={
+                <PrivateRoute redirectTo="/login" component={<Contacts />} />
+              }
+            ></Route>
+            <Route
+              path="/register"
+              element={
+                <RestrictedRoute
+                  redirectTo="/contacts"
+                  component={<ModalRegister />}
+                />
+              }
+            ></Route>
+            <Route
+              path="/login"
+              element={
+                <RestrictedRoute
+                  redirectTo="/contacts"
+                  component={<ModalLogin />}
+                />
+              }
+            ></Route>
+            <Route
+              path="/logout"
+              element={<PrivateRoute redirectTo="/" component={<Home />} />}
+            ></Route>
+          </Route>
 
-        <Route path="*" element={<p>Path not resolved</p>} />
-      </Routes>
+          <Route path="*" element={<p>Path not resolved</p>} />
+        </Routes>
+      )}
     </>
   );
 };
